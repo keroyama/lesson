2ee7578e309f95ccc0bbdaf54be4b43dd6de1de2
kaji <kanaiwork@gmail.com>
2016-04-17 13:48:20 +0900

    fomr2

--- js/form.js
+++ js/form.js
@@ -1,6 +1,15 @@
 angular.module('myApp', [])
-  .controller('MyController', ['$scope', function($scope) {
+.controller('MyController', ['$scope', function($scope) {
     $scope.onsubmit = function() {
       console.log('メールアドレス：' + $scope.user.mail);
     };
-  }]);
+
+    $scope.scalarValue = 'スカラー';
+    $scope.arrayValue = ['あいうえお', 'かきくけこ'];
+    $scope.hashValue = {name: '山田', sex: '男'};
+
+    $scope.greeting = "こんにちわ名無しさん"
+    $scope.onclick = function(){
+        $scope.greeting = 'こんにちわ' + $scope.myName + 'さん';
+    }
+}]);
